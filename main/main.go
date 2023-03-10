9a6a5871248447ec4ee37764dd6e9a9b2559180f
AlexAR <moltron_nano@hotmail.com>
2023-03-10 16:33:33 -0600

    feat: attaching methods to types and interfaces

--- main/main.go
+++ main/main.go
@@ -5,8 +5,26 @@ import "fmt"
 var x int
 
 type person struct {
-	Fname string
+	fname string
 	lname string
+}
+
+//method attached to person type
+func (p person) speak() {
+	fmt.Println(p.fname, p.lname, `says, "Good morning "`)
+}
+
+type secrentAgent struct {
+	identity          person
+	licenseToSabotage bool
+}
+
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
 }
 
 func main() {
@@ -17,7 +35,7 @@ func main() {
 	xi := []int{1, 3, 91, 5, 511, 21, 31}
 	fmt.Println(xi)
 	fmt.Println(xi[1])
-	fmt.Printf("%T", xi)
+	fmt.Println("%T", xi)
 
 	some_map := map[string]int{
 		"key1": 12121,
@@ -31,8 +49,26 @@ func main() {
 		"John",
 		"Locke",
 	}
+
+	ops1 := secrentAgent{
+		p1,
+		true,
+	}
+
 	fmt.Println(p1.lname)
-	fmt.Println(p1.Fname)
+	fmt.Println(p1.fname)
 	fmt.Printf("%T", p1)
+	fmt.Println("----------------")
+	p1.speak()
+	fmt.Println("----------------")
+	fmt.Println("-------Secret-Agent---------")
+	fmt.Println(ops1.identity.fname,
+		ops1.identity.lname,
+		"has license to sabotage?", ops1.licenseToSabotage)
+	ops1.identity.speak()
+
+	fmt.Println("-------Human-Interface---------")
+	saySomething(p1)
+	saySomething(ops1.identity)
 
 }
